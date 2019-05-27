6a5b1a6ffdb9913c81bde29c1588b9e4ed591516
unknown <you@example.com>
2019-05-27 15:18:50 -0400

    removed serverID; made dynamic dice roll

--- bot.py
+++ bot.py
@@ -3,22 +3,21 @@ import os
 from random import randint
 
 token = os.environ.get('TOKEN')
-servID = int(os.environ.get('SERVERID'))
+# servID = int(os.environ.get('SERVERID'))
 
 client = discord.Client()
 
 @client.event
 async def on_message(message):
-    id = client.get_guild(servID)
+    # id = client.get_guild(servID)
 
-    if message.content.find("!hello") != -1:
+    if message.content == '!hello':
         await message.channel.send("Hi")
-    elif message.content == '!users':
-        await message.channel.send(f"""# of Members: {id.member_count}""") 
-    elif message.content == '!d6':
-        await message.channel.send(randint(1,6))
-    elif message.content == '!d20':
-        await message.channel.send(randint(1,20))
+    # elif message.content == '!users':
+    #     await message.channel.send(f"""# of Members: {id.member_count}""") 
+    elif message.content[:2] == '!d' and message.content[2:].isdigit():
+        diceSide = int(message.content[2:])
+        await message.channel.send(randint(1,diceSide))
     elif message.content == '!coinflip':
         coin = randint(0,1)
         if coin == 0:
