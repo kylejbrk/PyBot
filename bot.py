741da0d7143cb71f60415e5565a4e75dd712bf8b
unknown <you@example.com>
2019-05-27 16:41:48 -0400

    updated !help command

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ servID = int(os.environ.get('SERVERID'))
 
 client = discord.Client()
 
+
 @client.event
 async def on_message(message):
     id = client.get_guild(servID)
@@ -16,38 +17,44 @@ async def on_message(message):
     if message.content == '!hello':
         await message.channel.send("Hi")
     elif message.content == '!help':
-        file = open('README.md', 'r')
-        data = file.read().splitlines()
-        file.close()
-
-        bCmd = False
-        commands = []
-        for line in data:
-            if line == '# Commands':
-                bCmd = True
-                continue
-            elif bCmd:
-                commands.append(line)
-                if line == '```' and len(commands) > 1:
-                    break
-                                    
-        await message.channel.send('\n'.join(commands))
+        await message.channel.send(getCommands())
     elif message.content == '!users':
-        await message.channel.send(f"""# of Members: {id.member_count}""") 
+        await message.channel.send(f"""# of Members: {id.member_count}""")
     elif message.content[:2] == '!d' and message.content[2:].isdigit():
         diceSide = int(message.content[2:])
-        await message.channel.send(randint(1,diceSide))
+        await message.channel.send(randint(1, diceSide))
     elif message.content == '!coinflip':
-        coin = randint(0,1)
+        coin = randint(0, 1)
         if coin == 0:
             await message.channel.send('Heads')
         else:
             await message.channel.send('Tails')
+
 
 @client.event
 async def on_member_join(member):
     for channel in member.server.channels:
         if str(channel) == 'general':
             await message.channel.send(f'''Welcome to the server {member.mention}''')
+
+
+def getCommands():
+    file = open('README.md', 'r')
+    data = file.read().splitlines()
+    file.close()
+
+    bCmd = False
+    commands = ['**Commands**']
+    for line in data:
+        if line == '# Commands':
+            bCmd = True
+            continue
+        elif bCmd:
+            commands.append(line)
+            if line == '```' and len(commands) > 2:
+                break
+    commands.append('**Author:**')
+    commands.append('https://github.com/kylejbrk/Rabbot/')
+    return('\n'.join(commands))
 
 client.run(token)
