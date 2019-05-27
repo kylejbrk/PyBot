94bf6ac3993c82968d8909b95c68ae0074f64887
unknown <you@example.com>
2019-05-27 16:22:14 -0400

    added dynamic list of commands from readme

--- bot.py
+++ bot.py
@@ -1,20 +1,39 @@
 import discord
 import os
 from random import randint
+from bs4 import BeautifulSoup as bs4
+from markdown import markdown
 
 token = os.environ.get('TOKEN')
-# servID = int(os.environ.get('SERVERID'))
+servID = int(os.environ.get('SERVERID'))
 
 client = discord.Client()
 
 @client.event
 async def on_message(message):
-    # id = client.get_guild(servID)
+    id = client.get_guild(servID)
 
     if message.content == '!hello':
         await message.channel.send("Hi")
-    # elif message.content == '!users':
-    #     await message.channel.send(f"""# of Members: {id.member_count}""") 
+    elif message.content == '!help':
+        file = open('README.md', 'r')
+        data = file.read().splitlines()
+        file.close()
+
+        bCmd = False
+        commands = []
+        for line in data:
+            if line == '# Commands':
+                bCmd = True
+                continue
+            elif bCmd:
+                commands.append(line)
+                if line == '```' and len(commands) > 1:
+                    break
+                                    
+        await message.channel.send('\n'.join(commands))
+    elif message.content == '!users':
+        await message.channel.send(f"""# of Members: {id.member_count}""") 
     elif message.content[:2] == '!d' and message.content[2:].isdigit():
         diceSide = int(message.content[2:])
         await message.channel.send(randint(1,diceSide))
